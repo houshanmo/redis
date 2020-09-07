a346d1ef3cceb07fca3d3cf19a361f565fb6b3c2
245039526 <245039526@qq.com>
2020-09-07 19:37:44 +0800

    wangyl

--- src/main/java/com/atguigu/demo.java
+++ src/main/java/com/atguigu/demo.java
@@ -1,7 +1,13 @@
 package com.atguigu;
 
+import org.junit.Test;
+
 public class demo {
     public static void main(String[] args) {
         System.out.println("第二个");
+    }
+    @Test
+    public void test1(){
+        System.out.println("hello");
     }
 }
